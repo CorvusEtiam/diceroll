c336fac2be289a755ee73eb2b6dff87c43a1d090
CorvusEtiam <misiek.rybicki@gmail.com>
2021-07-02 18:39:00 +0200

    [CODE][WIP] Untested: Pattern matcher for array rules

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 Simple diceroll game, inspired by OneLonelyCoder for learning Zig
 
+## Ideas and basic usage you can find here
+
++ Build Systems
+    - Multiple build options
+    - Usage of vcpkg
+    - Usage of external libraries
++ Working with C libraries
+    - Using external dependencies `raylib`
++ Parsing Command Arguments
++ Lightweight CLI input reading
++ Random number generation
++ Working with Zig :)
+
+
 ## Building and dependencies
 
 This project was tested on windows with current zig master `zig 0.9` version, but it _should_ build on other systems as well.
@@ -23,3 +37,6 @@ Below there is list of known `zig build <step>` steps:
 * `run -- -t gui` -- start GUI version (`--` allow passing arguments to executable)
 * `gui` -- start GUI version of app
 
+## On Progress
+
+There are still many unused pieces of code and `main.zig` is highly unorganized. I will clean it up, extending Gui version.
---
+++ src/patterns.zig
@@ -0,0 +1,57 @@
+pub const std = @import("std");
+// OneLonelyCoder Array pattern matcher
+
+// Match pattern of dices
+// Equal size of pattern and dice set is required
+// `[0-9]` and exact digit
+// `?` any digit
+// ????? - any 5 digits
+// NNNNN - any 5 repeating digits 
+// `N` - any digit
+// `NN` -
+pub fn patternMatch(data: []u8, pattern: []const u8) bool {
+    std.debug.assert(data.len == pattern.len);
+    std.sort.sort(u8, data, {}, std.sort.asc(u8));
+    var index: usize = 0;
+    var last_matched : u8 = 0;
+    var last_matched_used: bool = false;
+    while ( index < data.len ) {
+        switch ( pattern[index] ) {
+            '1' | '2' | '3' | '4' | '5' | '6' => {
+                if ( data[index] != pattern[index] - '0' ) {
+                    return false;
+                }
+            },
+            'N' => {
+                if ( !last_matched_used ) {
+                    last_matched = data[index];
+                    last_matched_used = true;
+                } else {
+                    if ( last_matched != data[index] ) {
+                        return false;
+                    }
+                } 
+            },
+            '?' => {
+                continue;
+            }
+        }
+
+        index += 1;
+    }
+
+    return true;
+}
+
+
+test "Pattern matching" {
+    const arr = [_]u8 { 1, 1, 1, 5, 6 };
+    // same 5
+    std.testing.expectEqual(patternMatch(arr, "NNNNN"), false);
+    // same 4
+    std.testing.expectEqual(patternMatch(arr, "NNNN?"), false);
+    // same 3
+    std.testing.expectEqual(patternMatch(arr, "NNN??"), true);
+    // straight
+    std.testing.expectEqual(patternMatch(arr, "12345") or patternMatch(arr, "23456"), true);    
+}
