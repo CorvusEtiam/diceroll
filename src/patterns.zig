158796714580b806d9f1e2875edc99c20f4668ec
CorvusEtiam <misiek.rybicki@gmail.com>
2021-07-04 15:12:13 +0200

    [RULES] Initial pattern matching system stub

--- src/patterns.zig
+++ src/patterns.zig
@@ -6,35 +6,35 @@ pub const std = @import("std");
 // `[0-9]` and exact digit
 // `?` any digit
 // ????? - any 5 digits
-// NNNNN - any 5 repeating digits 
+// NNNNN - any 5 repeating digits
 // `N` - any digit
 // `NN` -
 pub fn patternMatch(data: []u8, pattern: []const u8) bool {
     std.debug.assert(data.len == pattern.len);
     std.sort.sort(u8, data, {}, std.sort.asc(u8));
     var index: usize = 0;
-    var last_matched : u8 = 0;
+    var last_matched: u8 = 0;
     var last_matched_used: bool = false;
-    while ( index < data.len ) {
-        switch ( pattern[index] ) {
+    while (index < data.len) {
+        switch (pattern[index]) {
             '1' | '2' | '3' | '4' | '5' | '6' => {
-                if ( data[index] != pattern[index] - '0' ) {
+                if (data[index] != pattern[index] - '0') {
                     return false;
                 }
             },
             'N' => {
-                if ( !last_matched_used ) {
+                if (!last_matched_used) {
                     last_matched = data[index];
                     last_matched_used = true;
                 } else {
-                    if ( last_matched != data[index] ) {
+                    if (last_matched != data[index]) {
                         return false;
                     }
-                } 
+                }
             },
             '?' => {
                 continue;
-            }
+            },
         }
 
         index += 1;
@@ -43,9 +43,8 @@ pub fn patternMatch(data: []u8, pattern: []const u8) bool {
     return true;
 }
 
-
 test "Pattern matching" {
-    const arr = [_]u8 { 1, 1, 1, 5, 6 };
+    const arr = [_]u8{ 1, 1, 1, 5, 6 };
     // same 5
     std.testing.expectEqual(patternMatch(arr, "NNNNN"), false);
     // same 4
@@ -53,5 +52,5 @@ test "Pattern matching" {
     // same 3
     std.testing.expectEqual(patternMatch(arr, "NNN??"), true);
     // straight
-    std.testing.expectEqual(patternMatch(arr, "12345") or patternMatch(arr, "23456"), true);    
+    std.testing.expectEqual(patternMatch(arr, "12345") or patternMatch(arr, "23456"), true);
 }
